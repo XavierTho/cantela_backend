7ee440aab1b141dda8864b5a463485e30d54eb80
XavierTho <xavierathompson@gmail.com>
2025-02-02 13:46:22 -0800

    Update port number from 8887 to 8223

--- main.py
+++ main.py
@@ -367,4 +367,4 @@ if __name__ == "__main__":
         initGradeLog()
         initProfiles()
         # initChatlog()
-    app.run(debug=True, host="0.0.0.0", port="8887")
+    app.run(debug=True, host="0.0.0.0", port="8223")
